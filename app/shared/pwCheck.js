4b324a3ebbf70eda8b138fe24d40e9303cbd81df
Pawel Kutesko <pawel.kutesko@gmail.com>
2015-03-29 21:16:12 +0200

    sign in and register forms

--- app/shared/pwCheck.js
+++ app/shared/pwCheck.js
@@ -4,12 +4,12 @@ angular.module('theHive')
       require: 'ngModel',
       link: function (scope, elem, attrs, ctrl) {
         var firstPassword = '#' + attrs.pwCheck;
-        elem.add(firstPassword).on('keyup', function () {
+        elem.add(firstPassword).on('input', function () {
           scope.$apply(function () {
             var v = elem.val()===angular.element( document.querySelector( firstPassword ) ).val();
             ctrl.$setValidity('pwmatch', v);
           });
-        });
+        }); 
       }
     }
   }]);
